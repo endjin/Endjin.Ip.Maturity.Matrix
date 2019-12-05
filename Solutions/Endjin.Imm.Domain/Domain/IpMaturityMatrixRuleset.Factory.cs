7d9d41a4800d22335aacbbd5305bcbaf30759162
Ian Griffiths <Ian.Griffiths@endjin.com>
2019-12-05 07:22:19 +0000

    Get canonical rule definitions from GitHub repo

--- Solutions/Endjin.Imm.Domain/Domain/IpMaturityMatrixRuleset.Factory.cs
+++ Solutions/Endjin.Imm.Domain/Domain/IpMaturityMatrixRuleset.Factory.cs
@@ -2,13 +2,17 @@
 {
     using System.Collections.Generic;
     using System.IO;
-    using Endjin.Imm.Domain;
     using Endjin.Imm.Serialisation;
     using Newtonsoft.Json;
     using YamlDotNet.Serialization;
 
     public partial class IpMaturityMatrixRuleset
     {
+        /// <summary>
+        /// The URL for the canonical rule set definitions.
+        /// </summary>
+        public const string RuleSetDefinitionsUrl = "https://raw.githubusercontent.com/endjin/Endjin.Ip.Maturity.Matrix.RuleDefinitions/master/RuleSet.yaml";
+
         public static IpMaturityMatrixRuleset FromJson(string json) => JsonConvert.DeserializeObject<IpMaturityMatrixRuleset>(json, Converter.Settings);
 
         public static IpMaturityMatrixRuleset FromYaml(string yaml)
